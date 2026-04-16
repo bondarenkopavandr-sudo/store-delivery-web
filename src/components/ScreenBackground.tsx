5dc93c7ae17583a7cc7285a95a31083a9348a676
bondarenkopavandr-sudo <bondarenkopav.andr@gmail.com>
2026-04-16 18:35:42 +0300

    Delete src directory

--- src/components/ScreenBackground.tsx
+++
@@ -1,5 +0,0 @@
-import React, { PropsWithChildren } from "react";
-
-export const ScreenBackground: React.FC<PropsWithChildren> = ({ children }) => (
-  <div className="screen-background">{children}</div>
-);
--- src/utils/currency.ts
+++
@@ -1,2 +0,0 @@
-export const formatCurrency = (value: number): string =>
-  `${value.toLocaleString("ru-RU", { maximumFractionDigits: 0 })} ₽`;
